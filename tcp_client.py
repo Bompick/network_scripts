d3929fad8da149bca2a544a840b702df3a4b48ff
Bompick <r.a.bayramov@gmail.com>
2024-10-27 11:50:27 +0300

    tcp&udp clients, server

--- tcp_client.py
+++ tcp_client.py
@@ -1,13 +1,13 @@
 import  socket
 
 
-target_host = 'www.google.com'
-target_port = 80
+target_host = '0.0.0.0'
+target_port = 9999
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET - IP4 адрес, SOCK_STREAM - TCP
 client.connect((target_host, target_port))
 
-client.send(b'')
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
 response = client.recv(4096)
 
 print(response.decode())
